1bcd6a072d019056da8ecea424866b2e85075a02
root <c.onugha@alustudent.com>
2025-03-13 17:50:51 -0700

    python-input_output

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ and saves them to a JSON file.
 
 import sys
 from os.path import exists
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
